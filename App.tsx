6d2c5f055f7effe4f826f8b5a13191b5511c2b3c
Whitelistedd <geforcexxy@gmail.com>
2023-03-11 19:05:38 +0300

    added stack navigation with Auth navigation and a welcome screen

--- App.tsx
+++ App.tsx
@@ -3,16 +3,23 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'src/styles/theme'
+import AuthNavigator from 'src/navigators/Auth'
 
 const { Screen, Navigator } = createNativeStackNavigator()
 
 export default function App() {
+  const authenticated = false
+
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Navigator>
-          <Screen name="Home" component={Home} />
-        </Navigator>
+        {authenticated ? (
+          <Navigator>
+            <Screen name="Home" component={Home} />
+          </Navigator>
+        ) : (
+          <AuthNavigator />
+        )}
       </NavigationContainer>
     </ThemeProvider>
   )
