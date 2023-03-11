5f1d7ced7f2ee1765ac72da5919dc2a60e6e3931
Whitelistedd <geforcexxy@gmail.com>
2023-03-11 21:13:57 +0300

    removed useless code from app.tsx

--- App.tsx
+++ App.tsx
@@ -4,30 +4,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'src/styles/theme'
 import AuthNavigator from 'src/navigators/Auth'
-import { SplashScreen } from 'src/pages/SplashScreen'
 
 const { Screen, Navigator } = createNativeStackNavigator()
 
 export default function App() {
   const authenticated = false
 
-  const loading = true
-
   return (
     <ThemeProvider theme={theme}>
-      {loading ? (
-        <SplashScreen />
-      ) : (
-        <NavigationContainer>
-          {authenticated ? (
-            <Navigator>
-              <Screen name="Home" component={Home} />
-            </Navigator>
-          ) : (
-            <AuthNavigator />
-          )}
-        </NavigationContainer>
-      )}
+      <NavigationContainer>
+        {authenticated ? (
+          <Navigator>
+            <Screen name="Home" component={Home} />
+          </Navigator>
+        ) : (
+          <AuthNavigator />
+        )}
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
