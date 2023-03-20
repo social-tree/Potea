93e4d2057d1f2dc1ae3b082438bb4350b4bd7fbb
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-20 12:28:13 +0000

    added typography styles manually

--- src/styles/utils/typography/index.ts
+++ src/styles/utils/typography/index.ts
@@ -1,39 +1,79 @@
-import {
-  HeadingFontSize,
-  IHeadingProps,
-  IParagraphProps,
-  ParagraphFontSize,
-} from './typography.types'
 
-import { css } from 'styled-components'
+import { css } from 'styled-components/native'
 
 const primaryHeadingStyles = css`
   font-weight: 700;
   line-height: 120%;
 `
 
-export const generateHeading = ({ size, color }: IHeadingProps) => {
-  return css`
-    font-size: ${HeadingFontSize[size]};
-    ${primaryHeadingStyles};
-    color: ${color};
-  `
-}
+export const HeadingOne = css`
+  ${primaryHeadingStyles}
+  font-size: 48px;
+`
+export const HeadingTwo = css`
+  ${primaryHeadingStyles}
+  font-size: 40px;
+`
+
+export const HeadingThree = css`
+  ${primaryHeadingStyles}
+  font-size: 32px;
+`
+
+export const HeadingFour = css`
+  ${primaryHeadingStyles}
+  font-size: 24px;
+`
+
+export const HeadingFive = css`
+  ${primaryHeadingStyles}
+  font-size: 20px;
+`
+
+
+
+export const HeadingSix = css`
+  ${primaryHeadingStyles}
+  font-size: 18px;
+`
+
+
+
+
+
+
 
 const primaryParagraphStyles = css`
   line-height: 140%;
   letter-spacing: 0.2px;
 `
 
-export const generateParagraph = ({
-  fontWeight,
-  size,
-  color,
-}: IParagraphProps) => {
-  return css`
-    font-weight: ${fontWeight};
-    font-size: ${ParagraphFontSize[size]};
-    color: ${color};
-    ${primaryParagraphStyles}
-  `
-}
+export const ParagraphXL = css`
+  ${primaryParagraphStyles}
+  font-size: 18px;
+`
+
+export const ParagraphL = css`
+  ${primaryParagraphStyles}
+  font-size: 16px;
+
+`
+
+export const ParagraphM = css`
+  ${primaryParagraphStyles}
+  font-size: 14px;
+
+`
+
+export const ParagraphS = css`
+  ${primaryParagraphStyles}
+  font-size: 12px;
+
+`
+
+export const ParagraphXS = css`
+  ${primaryParagraphStyles}
+  font-size: 10px;
+
+`
+
