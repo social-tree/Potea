ff6c4537ae381722a954b05f8263afc23d1f6f7f
Whitelistedd <geforcexxy@gmail.com>
2023-04-14 15:40:58 +0300

    edit SplashScreen. changed loading component to new one

--- src/pages/SplashScreen/SplashScreen.styles.ts
+++ src/pages/SplashScreen/SplashScreen.styles.ts
@@ -1,13 +1,15 @@
 import styled from 'styled-components/native'
-import LottieView from 'lottie-react-native'
 import { Logo } from 'src/assets/svg/Logo'
+import { Loading } from 'src/assets/animations/Loading'
 
 export const StyledLogo = styled(Logo)`
   margin-top: 50%;
 `
 
-export const StyledLottieView = styled(LottieView)`
-  padding-top: 30%;
+export const StyledLoading = styled(Loading)`
+  padding-top: 40%;
+  width: 200px;
+  height: 200px;
 `
 
 export const Container = styled.View`
--- src/pages/SplashScreen/index.tsx
+++ src/pages/SplashScreen/index.tsx
@@ -1,7 +1,6 @@
 import { SafeAreaView } from 'react-native'
 import { StatusBar } from 'react-native'
-import { Container, StyledLogo, StyledLottieView } from './SplashScreen.styles'
-import { theme } from 'src/styles/theme'
+import { Container, StyledLogo, StyledLoading } from './SplashScreen.styles'
 
 export const SplashScreen = () => {
   return (
@@ -9,25 +8,7 @@ export const SplashScreen = () => {
       <StatusBar />
       <Container>
         <StyledLogo />
-        <StyledLottieView
-          autoPlay
-          style={{
-            width: 200,
-            height: 200,
-          }}
-          colorFilters={[
-            {
-              keypath: 'Shape Layer 2',
-              color: theme.primary[500],
-            },
-            {
-              keypath: 'Shape Layer 1',
-              color: theme.primary[500],
-            },
-          ]}
-          loop
-          source={require('src/assets/animations/loading.json')}
-        />
+        <StyledLoading />
       </Container>
     </SafeAreaView>
   )
