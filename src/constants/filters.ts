3b170d598f15afdafa95c4575180232f1c57191a
Whitelistedd <geforcexxy@gmail.com>
2023-06-10 17:19:09 +0300

    edit filters. added filters with their own ids

--- src/constants/filters.ts
+++ src/constants/filters.ts
@@ -1,1 +1,22 @@
-export const allFilters = ['All', 'Monstera', 'Aloe', 'Palm', 'Cactaceae']
+export const allFilters = [
+  {
+    id: 0,
+    filter: 'All',
+  },
+  {
+    id: 1,
+    filter: 'Monstera',
+  },
+  {
+    id: 2,
+    filter: 'Aloe',
+  },
+  {
+    id: 3,
+    filter: 'Palm',
+  },
+  {
+    id: 4,
+    filter: 'Cactaceae',
+  },
+]
