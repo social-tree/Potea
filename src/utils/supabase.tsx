b1ff7eea708233b479e5cff1ff84b3eaf773d320
Whitelistedd <geforcexxy@gmail.com>
2023-07-15 16:15:47 +0300

    added Database type to supabase

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -3,15 +3,20 @@ import 'react-native-url-polyfill/auto'
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@env'
 
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Database } from 'src/types/supabase'
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    storage: AsyncStorage as any,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-})
+export const supabase = createClient<Database>(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  {
+    auth: {
+      storage: AsyncStorage as any,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+)
 
 export const storageSupabaseURL = `${SUPABASE_URL}/storage/v1/object/public/avatars/`
